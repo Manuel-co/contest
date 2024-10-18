d6d6026f0cdc72a5e591b12b03515f87a475e6bf
Jonathan Ndamzi Destiny <130183995+itz-destiny@users.noreply.github.com>
2024-10-18 13:40:09 +0100

    All contest

--- src/components/page-placeholder.tsx
+++ src/components/page-placeholder.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PagePlaceholder({ pageName }: { pageName: string }) {
   return (
-    <div className="flex flex-1 py-4 h-screen sm:h-fit flex-col space-y-2 px-4">
+    <div className="flex flex-1 sm:h-fit flex-col space-y-2 px-4">
       <span className="font-bold text-3xl">{pageName}</span>
     </div>
   );
