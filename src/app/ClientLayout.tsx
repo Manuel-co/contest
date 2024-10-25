58f209f0667928bb3772a1332571594244e33896
Manuel-co <nwekeemmanuel2m@gmail.com>
2024-10-25 14:02:10 +0100

    loader update

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,15 +1,24 @@
-"use client"; // Ensure this is a client component
+'use client';
 
+// Ensure this is a client component
 import { useEffect, useState } from 'react';
+
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+
 import ContextProvider from '@/components/context-provider';
 import SideNav from '@/components/side-nav';
+
 import Header from './header';
 
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const pathname = usePathname();
   const [isMounted, setIsMounted] = useState(false);
-  
+
   useEffect(() => {
     setIsMounted(true); // Set mounted state to true on client
   }, []);
@@ -18,17 +27,26 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 
   return (
     <ContextProvider>
-      {isMounted && !isLoginPage && <Header />} {/* Render Header only after mount */}
-      <div className="flex">
-        {isMounted && !isLoginPage && <SideNav />} {/* Render SideNav only after mount */}
-        <div className="w-full overflow-x-auto">
-          <div className="sm:h-[calc(99vh-60px)] overflow-auto ">
-            <div className="w-full flex justify-center mx-auto overflow-auto h-[calc(100vh - 120px)] overflow-y-auto relative">
-              <div className="w-full md:max-w-6xl">{children}</div>
+      {isMounted ? (
+        <>
+          {!isLoginPage && <Header />}
+          <div className="flex">
+            {!isLoginPage && <SideNav />}
+            <div className="w-full overflow-x-auto">
+              <div className="sm:h-[calc(99vh-60px)] overflow-auto ">
+                <div className="w-full flex justify-center mx-auto overflow-auto h-[calc(100vh - 120px)] overflow-y-auto relative">
+                  <div className="w-full md:max-w-6xl">{children}</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+<div className="flex flex-col justify-center items-center h-screen ">
+  <div className="loader"></div>
+</div>
+
+      )}
     </ContextProvider>
   );
 }
